4e1c2d18809168ad576ffd4cf1480ea2116c24aa
thlgood <thlgood@yucoat.com>
2012-06-06 20:56:45 +0800

    modified:   io-task1/fread.c 	modified:   io-task1/open.c

--- io-task1/fread.c
+++ io-task1/fread.c
@@ -1,53 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
 
 #define BLOCK_SIZE 256
 
-void revCopy(char *strOne, char *strTwo, size_t size);
+void revCopy(char *dst, char *src, size_t size);
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
-	{
+	if (argc < 2) {
 		fputs("Too lss argument!", stderr);
 		exit(EXIT_FAILURE);
 	}
-	FILE *temp_fp = fopen("temp_file", "w");
-	FILE *fp_in   = fopen(argv[1], "r");
-	char buf_one[BLOCK_SIZE];
-	char buf_two[BLOCK_SIZE];
-	size_t count;
+	struct stat fileInfo;
+	FILE *fp = fopen(argv[1], "r");
 
-	char flag = 0;
-	while (count = fread(buf_one, 1, BLOCK_SIZE, fp_in))
+	char buf[BLOCK_SIZE];
+	char *stack = NULL;
+
+	size_t stackSize = -1;
+	size_t count = -1;
+	size_t stackTop = 0;
+
+	lstat(argv[1], &fileInfo);
+	stackSize = fileInfo.st_size;
+	stack = (char *)malloc(stackSize);
+
+	while (count = fread(buf, 1, BLOCK_SIZE, fp))
 	{
-		revCopy(buf_one, buf_two, count);
-		if (0 == flag)
-		{
-			fwrite(buf_two, 1, count, temp_fp);
-			flag++;
-		}
-		else
-		{
-			fseek(temp_fp, count, SEEK_SET);
-			fwrite(buf_two, 1, count, temp_fp);
-		}
-		if (count != BLOCK_SIZE)
-			break;
+		memcpy(&stack[stackTop], buf, count);
+		stackTop += count;
+		if (count != BLOCK_SIZE) break;
 	}
+	fclose(fp);
+	fp = fopen(argv[1], "w");
 
-	fclose(fp_in);
-	fclose(temp_fp);
-	remove(argv[1]);
-	rename("temp_file", argv[1]);
+	count = stackTop > BLOCK_SIZE ? BLOCK_SIZE : stackTop;
+	while (stackTop > 0)
+	{
 
+		revCopy(buf, &stack[stackTop-count], count);
+		fwrite(buf, 1, count, fp);
+		stackTop -= count;
+		if (stackTop < count)
+			count = stackTop;
+	}
+	fclose(fp);
 	return 0;
 }
-void revCopy(char *strOne, char *strTwo, size_t size)
+void revCopy(char *dst, char *src, size_t size)
 {
 	size_t temp;
 	for (temp = 0; temp < size; temp++)
 	{
-		strTwo[temp] = strOne[size-temp-1];
+		dst[temp] = src[size-temp-1];
 	}
 }
